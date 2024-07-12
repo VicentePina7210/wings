8b893251f7aa9b6f928eda8f5717a2bccc0f0c90
Vicentepina7210 <vpina3210@gmail.com>
2024-07-11 21:39:58 -0500

    Drew the image onto the screen, resized and rotated it as well.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 #the start of the wings project
 import pygame
+import os #this helps us find the path to our assets
 
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Wings") #Sets the title for the open window
 
-WHITE = (255, 255, 255) #RGB color code for white, used in line 18
+SPACE_SHIP_WIDTH = 75
+SPACE_SHIP_HEIGHT = 75
 
+WHITE = (255, 255, 255) #RGB color code for white, used in line 18
 FPS = 60 #Frames per second, used to control the speed of the game to ensure same speed across machines
+
+PLAYER_SHIP_IMAGE = pygame.image.load(
+    os.path.join('assets', 'images', 'ship.png'))#using this syntax and importing os, our assets can load on any OS b/c path directories may differ
+
+PLAYER_SHIP = pygame.transform.rotate(pygame.transform.scale(
+    PLAYER_SHIP_IMAGE, (SPACE_SHIP_WIDTH, SPACE_SHIP_HEIGHT)), 270) #rotates the image to the right and scales down
 
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(PLAYER_SHIP, (100,300)) #loads our ship onto the screen. THE ODRER IN WHICH ITS LOADED MATTERS
     pygame.display.update()
 
 # Starting below this comment; This is the the main loop of the game. 
