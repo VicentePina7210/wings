9438fbe699f5aeb4b11c5579cd65dba937f12e26
Vicentepina7210 <vpina3210@gmail.com>
2024-07-11 21:32:43 -0500

    These are some suggestions I have for reformatting the syntax in your file.

--- sprites.py
+++ sprites.py
@@ -1,13 +1,16 @@
 import pygame
+import os
+
 class ship(pygame.sprite.Sprite):
     def __init__(self):
         #Call the parent class constructor
         pygame.sprite.Sprite.__init__(self)
         super().__init__() # call the constructor of the parent class to initialize sprite
-        self.image = pygame.image.upload('assets/images/ship.png').convert_alpha() # holds the image of the sprite
+        self.image = pygame.image.load(os.path.join('assets', 'images', 'ship.png')).convert_alpha() # holds the image of the sprite
         self.rect = self.image.get_rect #creates a rectangle around the image to represent the 'space' occupied (?)
         self.rect.center = (400, 300) # sets the initial position to the center of the screen
         self.speed = 5 # the speed of movement so currently 5 pixels per update? may need to be fixed
+
 
     def update(self, keys):
         if keys[pygame.K_LEFT] and self.rect.left > 0:
