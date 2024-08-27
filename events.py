fd61a81ff84ac8466f56764c18f226cc4bd7cfcc
VicentePina7210 <vpina3210@gmail.com>
2024-08-27 11:39:29 -0500

    Fixed the bug regarding the asteroid still exploding the ship even after the asteroid has been destroyed

--- events.py
+++ events.py
@@ -1,21 +1,23 @@
 import pygame
 from sprites import *
 from config import *
+isalive = True
 
 def Collision(Ship, Asteroid):
-    if Ship.rect.colliderect(Asteroid.rect):
+    if Ship.rect.colliderect(Asteroid.rect) and not isalive:
         Ship.image = IMAGE_EXPLOSION
-
+        print("broken")
 
 def Projectile_collision(self, Asteroid):
         # Check for collisions between projectiles and the target rectangle
      for projectile in self.projectiles:
          if projectile.rect.colliderect(Asteroid.rect):
              projectile.kill()  # Remove the projectile if it collides
+             Asteroid.kill()
+             isalive = False
              Asteroid.image = IMAGE_EXPLOSION
              Asteroid.explosion_timer = pygame.time.get_ticks()  # Start explosion timer to limit duration of explosion
-             return True  # Collision detected
-     return False  # No collision detected
+             return isalive
     
 
 
