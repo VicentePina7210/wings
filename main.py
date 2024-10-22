a19d9bfce1bf634702d2eec9a8d3bc2e0a789cea
Vicentepina7210 <vpina3210@gmail.com>
2024-10-22 16:40:56 -0500

    created random asteroid generation

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sprites import *
 from config import *
 import math
 import time
+import random
+
 
 
 class Projectiles(pygame.sprite.Sprite):
@@ -16,7 +18,7 @@ class Projectiles(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-    def update(self):
+    def update(self ):
         self.rect.x += 5  # Move the projectile up
         if self.rect.left > WIDTH:
             self.kill()  # Remove the projectile if it goes off-screen
@@ -30,7 +32,7 @@ class Ship(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(pygame.transform.scale(self.image, (75, 75)), 270)
         self.rect = self.image.get_rect() #creates a rectangle around the image to represent the 'space' occupied (?)
         self.rect.center = (400, 300) # sets the initial position to the center of the screen
-        self.speed = 3 # the speed of movement so currently 3 pixels per update? may need to be fixed
+        self.speed = 5 # the speed of movement so currently 3 pixels per update? may need to be fixed
         self.projectiles = pygame.sprite.Group()
         self.last_shot_time = 0 # track the time since last shot
         self.shoot_cooldown = 0.2
@@ -63,24 +65,29 @@ class Ship(pygame.sprite.Sprite):
         
 
 class Asteroid(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x_random):
         # Call the parent class constructor
         pygame.sprite.Sprite.__init__(self)
         super().__init__()  # call the constructor of the parent class to initialize sprite
         self.image = pygame.image.load(os.path.join('assets', 'images', 'asteroid.png')).convert_alpha()  # holds the image of the sprite
         self.image = pygame.transform.rotate(pygame.transform.scale(self.image, (75, 75)), 270)
         self.rect = self.image.get_rect()
-        self.rect.center = (800, 300)
+        self.rect.center = (800,x_random)
         self.last_update = pygame.time.get_ticks()  # track the time since last shot
         self.update_delay = 0  # time between movements in milliseconds
         self.update_x = 0  # make these instance variables
         self.update_y = 0  # make these instance variables
         self.explosion_timer = 0
+        self.health = 3
 
     def check_collision(self, other):
         hits = pygame.sprite.spritecollide(self, other, True)  # True removes projectile on collision
         if hits:
             print("Asteroid hit!")
+            self.health -= 1
+        if self.health == 1:
+            self.image = IMAGE_ASTEROID_CRITICAL
+        if self.health <= 0:
             self.kill()  # Remove asteroid if hit
 
     def update(self):
@@ -106,12 +113,8 @@ class Asteroid(pygame.sprite.Sprite):
             self.update_x = -1  # move left
             #move the self rect x and y into variables outside the update fuinction
 
-    
 
 
-
-
-        
 
 pygame.display.set_caption("Wings") #Sets the title for the open window
 
@@ -120,7 +123,6 @@ FPS = 60 #Frames per second, used to control the speed of the game to ensure sam
     
 #Create an instance of the ship class
 player = Ship()
-Asteroid_instance = Asteroid()
 
 #Create sprite group and add the player sprite to it
 all_sprites = pygame.sprite.Group()
@@ -128,13 +130,22 @@ all_sprites.add(player)
 
 
 asteroid_group = pygame.sprite.Group()
-asteroid_group.add(Asteroid_instance)
 
 background_width = background_image.get_width()
 
 tiles = math.ceil(WIDTH / background_width) +1
 print(tiles)
 scroll = 0
+# Here we create the instance of the asteroid 
+def draw_asteroids():
+    if len(asteroid_group) < 1:
+        x_random = random.randint(50, 550)
+        asteroid = Asteroid(x_random)
+        asteroid_group.add(asteroid)
+        all_sprites.add(asteroid)
+        
+
+
 #Draw onto screen
 def draw_window():
     # WIN.fill(WHITE)
@@ -142,6 +153,7 @@ def draw_window():
     all_sprites.draw(WIN)
     player.projectiles.draw(WIN) # testing code
     asteroid_group.draw(WIN)  # Draw the asteroids
+
     pygame.display.update()
 
 def main():
@@ -161,6 +173,8 @@ def main():
         if abs(scroll) > background_width:
             scroll = 0
 
+        
+
 
 
         for asteroid in asteroid_group:
@@ -168,6 +182,9 @@ def main():
 
         asteroid_group.update()
         all_sprites.update()
+        
+        draw_asteroids()
+        
         draw_window()#calls function from line 10
         pygame.display.update()
         pygame.display.flip()
